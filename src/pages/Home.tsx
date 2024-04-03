05c2ce9a09fb08fb482de7a079c49ade4f938dc1
Rayhan-abdullha <captainrayhan215@gmail.com>
2024-04-03 11:14:58 +0600

    delete section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,9 @@ import FeaturesProducts from "../components/home/FeaturesProducts"
 import FeaturedCategory from "../components/home/FeaturedCategory"
 import Slider from "../components/home/Slider"
 import Services from "../components/home/Services"
-import { useRef } from 'react';
-
 const Home = () => {
-    const scrollRef = useRef<HTMLDivElement>(null);
-
-    const scrollToTop = () => {
-        scrollRef.current?.scrollIntoView({ behavior: 'smooth' });
-    };
-
     return (
         <div className="max-w-[1290px] mx-auto px-4 pt-4 mt-14 mg:mt-12 lg:mt-0 md:pt-7 pb-[60px] md:pb-[100px]">
-            <div ref={scrollRef} />
-
             <Slider />
             <Services />
             <section className="sliding_text rounded-full p-2 bg-white mt-7">
@@ -22,8 +12,6 @@ const Home = () => {
             </section>
             <FeaturedCategory />
             <FeaturesProducts />
-
-            <button onClick={scrollToTop}>Scroll to Top</button>
         </div>
     )
 }
