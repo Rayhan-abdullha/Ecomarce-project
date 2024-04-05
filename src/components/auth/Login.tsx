e0cc105a7306e07b53eef7f2a96b7d59f4ce56d3
Rayhan-abdullha <captainrayhan215@gmail.com>
2024-04-05 14:20:50 +0600

    add to cart page added, and cart drawer cart added and modal added

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-const Login = ({ handleToggle }: { handleToggle: React.Dispatch<React.SetStateAction<boolean>> }) => {
+const Login = () => {
     return (
-        <div>
-            login
-            
-            <button onClick={handleToggle}>Create your accout</button>
+        <div className="max-w-[500px] mx-auto px-4 py-[100px]">
+            <h2 className="text-[18px] uppercase mb-3 text-center">Login</h2>
+            <form className="flex flex-col gap-4">
+                <input className="outline-0 p-2 px-4" type="text" placeholder="Enter yor email" readOnly/>
+                <input className="outline-0 p-2 px-4" type="text" placeholder="Enter yor password" readOnly />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
-
 export default Login
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,14 +1,9 @@
-import { useState } from "react"
 import Login from "../components/auth/Login"
-import Registration from '../components/auth/Registration'
 
 const AuthPage = () => {
-    const [toggle, setToggle] = useState<boolean>(false)
     return (
         <div>
-            {
-                toggle ? <Login handleToggle={setToggle}/> : <Registration handleToggle={setToggle}/>
-            }
+            <Login/>    
         </div>
     )
 }
